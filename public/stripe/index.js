b14c73083b2009042df8c31663244188b3fbb6f6
korman <korman.yuri@gmail.com>
2017-10-10 20:29:49 +0300

    fixed

--- public/stripe/index.js
+++ public/stripe/index.js
@@ -47,44 +47,55 @@ function registerElements(elements, exampleName) {
 
   // Listen on the form's 'submit' handler...
   form.addEventListener('submit', function(e) {
-    e.preventDefault();
+      e.preventDefault();
 
-    // Show a loading screen...
-    example.classList.add('submitting');
+      // Show a loading screen...
+      example.classList.add('submitting');
 
-    // Disable all inputs.
-    disableInputs();
+      // Disable all inputs.
+      disableInputs();
 
-    // Gather additional customer data we may have collected in our form.
-    var name = form.querySelector('#' + exampleName + '-name');
-    var address1 = form.querySelector('#' + exampleName + '-address');
-    var city = form.querySelector('#' + exampleName + '-city');
-    var state = form.querySelector('#' + exampleName + '-state');
-    var zip = form.querySelector('#' + exampleName + '-zip');
-    var additionalData = {
-      name: name ? name.value : undefined,
-      address_line1: address1 ? address1.value : undefined,
-      address_city: city ? city.value : undefined,
-      address_state: state ? state.value : undefined,
-      address_zip: zip ? zip.value : undefined,
-    };
+      // Gather additional customer data we may have collected in our form.
+      var name      = form.querySelector('#' + exampleName + '-name');
+      var address1  = form.querySelector('#' + exampleName + '-address');
+      var city      = form.querySelector('#' + exampleName + '-city');
+      var state     = form.querySelector('#' + exampleName + '-state');
+      var zip       = form.querySelector('#' + exampleName + '-zip');
 
-    // Use Stripe.js to create a token. We only need to pass in one Element
-    // from the Element group in order to create a token. We can also pass
-    // in the additional customer data we collected in our form.
-    stripe.createToken(elements[0], additionalData).then(function(result) {
-      // Stop loading!
-      example.classList.remove('submitting');
+      var additionalData = {
+          name: name ? name.value : undefined,
+          address_line1: address1 ? address1.value : undefined,
+          address_city: city ? city.value : undefined,
+          address_state: state ? state.value : undefined,
+          address_zip: zip ? zip.value : undefined,
+      };
 
-      if (result.token) {
-        // If we received a token, show the token ID.
-        //example.querySelector('.token').innerText = result.token.id;
-        example.classList.add('submitted');
-      } else {
-        // Otherwise, un-disable inputs.
-        enableInputs();
-      }
-    });
+      // Use Stripe.js to create a token. We only need to pass in one Element
+      // from the Element group in order to create a token. We can also pass
+      // in the additional customer data we collected in our form.
+      stripe.createToken(elements[0], additionalData).then(function(result) {
+
+          // Stop loading!
+          example.classList.remove('submitting');
+
+          if (result.token) {
+              // If we received a token, show the token ID.
+              //example.querySelector('.token').innerText = result.token.id;
+              example.classList.add('submitted');
+
+              var hiddenInput = document.createElement('input');
+              hiddenInput.setAttribute('type', 'hidden');
+              hiddenInput.setAttribute('name', 'stripeToken');
+              hiddenInput.setAttribute('value', result.token.id);
+              form.appendChild(hiddenInput);
+
+              form.submit();
+          } else {
+              // Otherwise, un-disable inputs.
+              enableInputs();
+          }
+
+      });
   });
 
   // resetButton.addEventListener('click', function(e) {
