d83aa93b958b49f84392ca854127d62571f4f429
korman <korman.yuri@gmail.com>
2017-10-13 23:05:06 +0300

    changed payment

--- src/js/plugin/components/gift_card/GiftCardFriendBuy.js
+++ src/js/plugin/components/gift_card/GiftCardFriendBuy.js
@@ -39,7 +39,7 @@ export default class GiftCardModal extends React.Component {
 
     render() {
         if (this.state.item) {
-            console.log(this.state.item.ownerConsumer.socialDataProfile);
+            console.log(this.state.item.ownerConsumer.socialDataProfile.nickname);
             const nickname = this.state.item.ownerConsumer.socialProfileData.nickname;
 
             return (
