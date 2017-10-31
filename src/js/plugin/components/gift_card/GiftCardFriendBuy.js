5f4b5b9b444b9020100f8ddf8bbdb09ba3254cb0
korman <korman.yuri@gmail.com>
2017-11-01 00:50:49 +0200

    fixed bug

--- src/js/plugin/components/gift_card/GiftCardFriendBuy.js
+++ src/js/plugin/components/gift_card/GiftCardFriendBuy.js
@@ -77,31 +77,33 @@ export default class GiftCardFriendBuy extends React.Component {
 
     timer(){
 
-        // Get todays date and time
-        let now = new Date().getTime();
+        if (this.state.item) {
+            // Get todays date and time
+            let now = new Date().getTime();
 
-        // Find the distance between now an the count down date
-        let distance = this.state.countDownDate - now;
+            // Find the distance between now an the count down date
+            let distance = this.state.countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
-        let days    = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours   = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            // Time calculations for days, hours, minutes and seconds
+            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Display the result in the element with id="demo"
-        if (!isNaN(days)) {
-            this.setState({
-                timeLeft: days + " days " + hours + ":" + minutes + ":" + seconds
-            });
-        }
-        // console.log(this.state.timeLeft);
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(this.interval);
-            this.setState({
-                timeLeft: "EXPIRED"
-            });
+            // Display the result in the element with id="demo"
+            if (!isNaN(days)) {
+                this.setState({
+                    timeLeft: days + " days " + hours + ":" + minutes + ":" + seconds
+                });
+            }
+            // console.log(this.state.timeLeft);
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(this.interval);
+                this.setState({
+                    timeLeft: "EXPIRED"
+                });
+            }
         }
     }
 
